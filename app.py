c3b823dbd22af0b4d59743e11ac2f5b82c4b2c8b
Cleocelestino <cleocelestinoo@gmail.com>
2021-11-29 13:13:04 -0300

    Desenhando 1Quadrado

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from sys import exit
 
 pygame.init()
 
+NOME_JOGO = "Caixas"
 LARGURA = 640
 ALTURA = 480
+TELA = (LARGURA, ALTURA)
 COR_PRETA = (0,0,0)
 
-tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set_caption("Caixas")
+tela = pygame.display.set_mode(TELA)
+pygame.display.set_caption(NOME_JOGO)
 
 while True:
     tela.fill(COR_PRETA)
@@ -20,7 +22,12 @@ while True:
             pygame.quit()
             exit()
 
+    pygame.draw.rect(tela, (255,0,255), (200,300,40,50))
             
+    pygame.display.update()
+
+
+      
 
     
 
