045269759a76872d55f3e91b307df139765ffed5
Cleocelestino <cleocelestinoo@gmail.com>
2021-11-29 13:24:17 -0300

    Adicionando circulo

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ LARGURA = 640
 ALTURA = 480
 TELA = (LARGURA, ALTURA)
 COR_PRETA = (0,0,0)
+COR_AZUL = (0,0,120)
+COR_ROSA = (255,0,255)
 
 tela = pygame.display.set_mode(TELA)
 pygame.display.set_caption(NOME_JOGO)
@@ -22,11 +24,11 @@ while True:
             pygame.quit()
             exit()
 
-    pygame.draw.rect(tela, (255,0,255), (200,300,40,50))
-            
+    pygame.draw.rect(tela, COR_AZUL, (200,300,40,50))
+    pygame.draw.circle(tela, COR_ROSA, (100, 130), 10)
+
+
     pygame.display.update()
-
-
       
 
     
