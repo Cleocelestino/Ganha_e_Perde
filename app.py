ba248886728de1d67655a245f2f64fad38f2a27b
Cleocelestino <cleocelestinoo@gmail.com>
2021-11-29 16:00:31 -0300

    Adicionando Relogio

--- app.py
+++ app.py
@@ -12,10 +12,15 @@ COR_PRETA = (0,0,0)
 COR_AZUL = (0,0,120)
 COR_ROSA = (255,0,255)
 
+x = 0
+y = 0
+
 tela = pygame.display.set_mode(TELA)
 pygame.display.set_caption(NOME_JOGO)
+relogio = pygame.time.Clock()
 
 while True:
+    relogio.tick(10)
     tela.fill(COR_PRETA)
 
     for event in pygame.event.get():
@@ -24,9 +29,14 @@ while True:
             pygame.quit()
             exit()
 
-    pygame.draw.rect(tela, COR_AZUL, (200,300,40,50))
+    pygame.draw.rect(tela, COR_AZUL, (x,y,40,50))
     pygame.draw.circle(tela, COR_ROSA, (100, 130), 10)
+    if y >= ALTURA:
+        y = 0 
+    y = y + 1
+     
 
+    
 
     pygame.display.update()
       
